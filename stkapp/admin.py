f33ec04c299a8d54683096f4faeb0b347d76c741
josephbill <josephbill00@gmail.com>
2025-05-29 16:55:18 +0300

    added necessary view functions

--- stkapp/admin.py
+++ stkapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Transaction
 # Register your models here.
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('transaction_id', 'phone_number','amount','status',
+                    'mpesa_receipt_number', 'date_created','transaction_date')
+    list_filter = ('status', 'date_created' ,'transaction_date')
+    search_fields = ('transaction_id','phone_number','mpesa_receipt_number')
+
---
+++ stkapp/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'), # front page
+    #path to prompt stk push
+    path('', views.stk_push, name='stk_push'),
+    # the routes for status checking
+    path('waiting/<int:transaction_id>/', views.waiting_page, name="waiting_page"),
+   # this is the route that will receive the status of our transaction
+    path('callback', views.callback, name="callback"),
+    # this route confirms the status from above callback result
+    path('check-status/<int:transaction_id>/', views.check_status, name="check-status"),
+    # payments views (successful, failed or cancelled)
+    path('payment-success/', views.payment_success, name="payment-success"),
+    path('payment-failed/', views.payment_failed, name="payment-failed"),
+    path('payment-cancelled/', views.payment_cancelled, name="payment-cancelled"),
+]
